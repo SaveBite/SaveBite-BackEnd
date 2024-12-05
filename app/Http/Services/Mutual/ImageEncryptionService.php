5d3c20bcedf791ddd6e4192d4cc850c2b751f11a
MostafaAbdelmajied <mostafa.abdo2029@gmail.com>
2024-12-05 16:15:29 +0200

    image encryption dervice

---
+++ app/Http/Services/Mutual/ImageEncryptionService.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Http\Services\Mutual;
+
+use App\Repository\EncodedImageRepositoryInterface;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+
+class ImageEncryptionService
+{
+    public function __construct(private EncodedImageRepositoryInterface $encodedImageRepository){}
+    public function embed($requestAttributeName, $email, $password){
+        $data = [
+            'email' => $email,
+            'password' => $password,
+        ];
+
+        $response = Http::attach(
+            'image',
+            file_get_contents(request()->file($requestAttributeName)->getRealPath()),
+            request()->file($requestAttributeName)->getClientOriginalName(),
+        )->post(config('imageEncryptionApi.base_url') . 'embed/', $data);
+
+        if(! $response->successful())
+            throw new \Exception("Failed to send request to external api");
+        $responseImage = Http::get(config("imageEncryptionApi.base_url") . $response->json()['image_url']);
+        if(! $responseImage)
+            throw new \Exception("Failed to download Image");
+        $content = $responseImage->body();
+        $imageName = "image_" . time() . ".png";
+        $path = "encoded_images/" . $imageName;
+        Storage::disk('public')->put($path,$content);
+        $data = [
+            'email' => $email,
+            'image' => $path,
+            'token' => $response->json()['fernet_key']
+        ];
+        $image = $this->encodedImageRepository->create($data);
+        return true;
+    }
+
+    public function extract($requestAttributeName, $key){
+        $response = Http::attach(
+            'image',
+            file_get_contents(request()->file($requestAttributeName)->getRealPath()),
+            request()->file($requestAttributeName)->getClientOriginalName()
+        )->post(config("imageEncryptionApi.base_url") . "extract/",["fernet_key" => $key]);
+        if(! $response->successful())
+            throw new \Exception("Failed to send request to external api");
+        return [
+            "email" => $response->json()['email'],
+            "password" => $response->json()['password'],
+        ];
+    }
+}
---
+++ config/imageEncryptionApi.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "base_url" => env("IMAGE_ENCRYPTION_API_URL", "http://34.27.32.70:8000/")
+];
